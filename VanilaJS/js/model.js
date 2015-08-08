9cad52d40b26d6b65779b4cb504a96cabd3cb09e
TehreemAnsari <TehreemAnsari@users.noreply.github.com>
2015-08-08 22:38:05 +0530

    Create model.js

---
+++ VanilaJS/js/model.js
@@ -0,0 +1,24 @@
+var Model = function () {
+        var _todos = new Array();    
+
+  var notifyController = function () {  
+            $('body').trigger('updateView');
+  }
+        // public methods
+  return  {
+    addTodo: function ( todo ) {
+        _todos.push(todo);
+       notifyController();
+    },
+    editTodo: function( index, newTodo ){
+        _todos[index] = newTodo;
+    },
+    deleteTodo: function ( index ) {
+       _todos.splice(index,1);
+       notifyController();
+    },
+    getData: function(){
+        return _todos;
+    }
+  };
+};
